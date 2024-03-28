8b35189bcfe8016e9a4ca1cf45dd4c4339a5379c
Sebastian D. Eastham <sdeastham@googlemail.com>
2024-03-28 15:41:53 +0000

    Added simple great circle distance calculator

--- PhysConstants.cs
+++ PhysConstants.cs
@@ -5,4 +5,5 @@ public static class PhysConstants
     public const double RGasUniversal = 8.3144598; // J/K/mol
     public const double G0 = 9.80665; // m/s2
     public const double Avogadro = 6.022140857e23; // molec/mol
+    public const double EarthRadius = 6.3710072e3; // km, giving a sphere with same overall area as Earth spheroid 
 }
