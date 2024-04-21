1001d51c5c4900247a1ca7daa4f98358ac8e74fc
Sebastian D. Eastham <sdeastham@googlemail.com>
2024-04-21 09:29:39 +0100

    New class to handle ideal gas relationships

---
+++ IdealGases.cs
@@ -0,0 +1,15 @@
+﻿namespace AtmosTools;
+
+public static class IdealGases
+{
+    public static readonly double GammaAir = 1.4;
+    public static readonly double CpAir = 1005.0; // J/kg/K
+    public static readonly double MolarMassAir = 28.97e-3; // kg/mol
+
+    // Precomputed factor for convenience
+    private static readonly double MachFactor = GammaAir * PhysConstants.RGasUniversal / MolarMassAir; 
+    public static double SpeedOfSound(double temperature)
+    {
+        return Math.Sqrt(temperature * MachFactor);
+    }
+}
